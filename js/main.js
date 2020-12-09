2f8e7880a7af45b792ec118eddfe8706122ea87c
Amambi cyprain <cypriannjeck@gmail.com>
2020-12-09 23:43:41 +0100

    added scrolltriger effect to home page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+
 $( document ).ready(function() {
+    gsap.registerPlugin(ScrollTrigger);
     const headerTextMove = gsap.timeline();
     const showcols = gsap.timeline();
-    const zoomIn = gsap.timeline();
+    const zoomIn = gsap.timeline({});
+    const textUp = gsap.timeline();
     headerTextMove.fromTo("#headerText", {
         display:"none",
         x:-200,opacity:0}, 
@@ -11,20 +14,81 @@ $( document ).ready(function() {
             display:"block",
             ease:"power2.out"
         });
-    showcols.fromTo("#flipCols", {opacity:0, 
-        rotationY: 100,
-        rotationX: 100 ,
+    showcols.fromTo("#flipCols", 
+        {
+         opacity:0, 
+         rotationY: 100,
+         rotationX: 100 ,
          skewY:-60,
          scale:0.8,
          rotationZ: 60}, 
-         {duration: 0.8, 
+         {
+            duration: 4, 
             scale:1, 
             skewY:0, 
             opacity:1, 
             rotationX: 0,
             rotationZ: 0, 
             rotationY: 0,
-            ease:"power2.out" 
+            ease:"power2.out",
+            scrollTrigger:
+            { 
+             trigger: ".animTriger",
+             markers: false,
+             start:"top top+=5%",
+             end:"bottom top",
+             scrub:true}
          });
-    zoomIn.fromTo("#zoomIn", {scale:0}, {duration: 0.7, scale:1,ease:"power2.out" })
+    zoomIn.fromTo("#zoomIn",
+     {scale:0},
+      {
+        duration: 4,
+        scale:1,
+        ease:"power2.out" ,
+     scrollTrigger:
+     { 
+      trigger: ".animTriger",
+      markers: false, 
+      start:"top top+=5%",
+      end:"bottom top",
+      scrub:true
+    }
+})
+    textUp.fromTo("#headingUP", 
+    {
+        y:100,
+        opacity:0}, 
+        {
+          duration: 1, 
+          y:0,
+          opacity:1,
+          ease:"power2.out",
+          scrollTrigger:
+          { 
+           trigger: ".animTriger",
+           markers: false, 
+           start:"top top+=15%",
+           end:"bottom top",
+           scrub:true
+         }
+        })
+    textUp.fromTo("#ParaUP",
+     {
+         y:150,
+        opacity:0
+    }, 
+    {
+      duration: 1,
+      y:0,
+      opacity:1,
+      ease:"power2.out",
+      scrollTrigger:
+      { 
+       trigger: ".animTriger",
+       markers: false, 
+       start:"top top+=10%",
+       end:"bottom top",
+       scrub:true
+     }
+    })
 });
